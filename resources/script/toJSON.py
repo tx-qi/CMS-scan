52b1e29248cd1b3dda1d9889becc8413c495c05b
Taqi <ahmadtaqiyuddin98@yahoo.com>
2021-06-14 15:18:26 +0800

    first push

---
+++ resources/script/toJSON.py
@@ -0,0 +1,53 @@
+import json
+import re
+import os
+
+fff = open("apu.edu.my.txt", "r")
+countLow = 0
+countMed = 0
+countHigh = 0
+countTotal = 0
+lowVuln = []
+medVuln = []
+highVuln = []
+cmsInfo = []
+
+c = fff.readlines()
+
+for i in c:
+    line = i[3::].strip()
+
+    if i[1] == "L":
+        lowVuln.append(line)
+        countLow += 1
+
+    elif i[1] == "M":
+        medVuln.append(line)
+        countMed += 1
+
+    elif i[1] == "H":
+        highVuln.append(line)
+        countHigh += 1
+
+    elif i[1] == "I":
+        cmsInfo.append(line)
+
+countTotal= countLow + countMed + countHigh
+
+data = {'data': [{'Low-vulnerability': lowVuln, 'Medium-vulnerability': medVuln, 'High-vulnerability': highVuln, 'Cms-info': cmsInfo, 'countLow': countLow, 'countMed': countMed, 'countHigh': countHigh, 'countTotal': countTotal}]}
+jsondata = json.dumps(data, indent=4)
+
+fName = c[0]
+a = fName.split()
+
+filename = a[1]
+url = re.compile(r"https?://(www\.)?")
+
+filenames = url.sub('', filename).strip().strip('/')
+filenames = filenames + ".json"
+filepath = '..\scanJSON'
+completeName=os.path.join(filepath, filenames)
+
+fff = open(completeName,"w+")
+fff.write(jsondata)
+fff.close()
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,39 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    You're logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+     <div>
+        <div class="wrap">
+            <div class="search" style="text-align: center">
+               <input type="text" class="searchTerm" placeholder="Please enter website"> <br>
+               <input type="radio" id="Scan" name="fullScan" value="fullScan">
+                <label for="fullScan">Full Scan</label>
+                <input type="radio" id="Scan" name="defaultScan" value="defaultScan">
+                <label for="defaultScan"> Default Scan</label>
+                <label for="typeOfCMS">| Type of CMD:</label>
+                <input list="typeOfCMS" name="typeOfCMS" id="typeOfCMS">
+                <datalist id="typeOfCMS">
+                  <option value="Auto">
+                  <option value="Wordpress">
+                  <option value="Drupal">
+                  <option value="Joomla">
+                  <option value="Moodle">
+                </datalist>
+               <button type="submit" class="searchButton">
+                 <i class="fa fa-search"></i>
+              </button>
+            </div>
+     </div> </div>
+</x-app-layout>
